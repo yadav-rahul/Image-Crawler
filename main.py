a7893bfafd57143b7d52ae91b17b92aa31991da7
Rahul Yadav <rahuly247@gmail.com>
2016-06-20 02:39:51 +0530

    Minor Bug Fixed

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import os
 import sys
-import random
 import urllib.request
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, redirect
 
 ic = Flask(__name__)
+
+count = 0
 
 
 @ic.route("/")
 def main():
-    return render_template("index.html")
+    if count == 1:
+        return render_template("index.html", result=str((str(count) + " Image Downloaded !")))
+    else:
+        return render_template("index.html", result=str((str(count) + " Images Downloaded !")))
 
 
 @ic.route("/get_images", methods=['POST'])
 def get_images():
     _url = request.form['inputURL']
     try:
+        global count
         count = 0
         code = requests.get(_url)
         text = code.text
@@ -30,17 +35,14 @@ def get_images():
             else:
                 src = urljoin(_url, img.get('src'))
             download_image(src, count)
-        if count == 1:
-            return render_template("index.html", result=str((str(count) + " Image Downloaded !")))
-        else:
-            return render_template("index.html", result=str((str(count) + " Images Downloaded !")))
+        return redirect("http://localhost:5000")
     except requests.exceptions.HTTPError as error:
         return render_template("index.html", result=str(error))
 
 
-def download_image(url, count):
+def download_image(url, num):
     try:
-        image_name = str(count) + ".jpg"
+        image_name = str(num) + ".jpg"
         image_path = os.path.join("images/", image_name)
         urllib.request.urlretrieve(url, image_path)
     except ValueError:
